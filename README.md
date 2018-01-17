7a3b7e4e383ee744d7541dbe6f90a1565b7beb6d
theheroGAC <gamesandconsolestaff@gmail.com>
2018-01-17 21:49:32 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# Cross-Connection
+# TubeVita
+Simple application to use youtube on PSVITA
+Download the vpk file, install with vitashell. You do not need to register on Youtube to watch the videos 
+# Changelog: 
+v1.01
+-Added splash screen
+-Added some initial code for the next releases
+-ordered code
+v1.00 
+-First Release
+# Credits
+- Team OneLua
+- TheFloW Pkg installer & USB Modules.
+- Yifan-lu, XYZ and Davee and every coder and dev contributing to Vitasdk.
+- Team Molecule for Henkaku.
---
+++ script.lua
@@ -0,0 +1,52 @@
+--[[ 
+	ONElua.
+	Lua Interpreter for PlayStation®Vita.
+	
+	Licensed by GNU General Public License v3.0
+	
+	Copyright (C) 2014-2018, ONElua Team
+	http://onelua.x10.mx/staff.html
+	
+	Designed By:
+	- Gdljjrod (https://twitter.com/gdljjrod).
+	- DevDavisNunez (https://twitter.com/DevDavisNunez).
+
+]]
+
+color.loadpalette() -- Load Defaults colors
+
+-- ## IMMAGINE DI SFONDO ##
+back = image.load("resources/back.png")
+dofile("resources/usb.lua")
+
+while true do
+	buttons.read()
+	if back then back:blit(0,0) end
+	
+	screen.print(10,20,"To create a cross connection press CROSS to start the FTP and press Triangle to start USB.",1,color.red)
+	screen.print(360,160,"Press CROSS to start ftp.",1,color.white,color.blue)
+	screen.print(360,190,"Press TRIANGLE to connect USB.",1,color.white,color.blue)
+	screen.print(360,220,"Press SQUARE to restart HB.",1,color.white,color.blue)
+	screen.print(360,250,"Press START to exit.",1,color.white,color.blue)
+	screen.print(360,280, "Mac: "..tostring(os.mac()))
+
+	if ftp.state() then	screen.print(360,320,"Connect to:\nftp://"..tostring(wlan.getip())..":1337",1,color.green) end
+
+	screen.flip() -- Show Buff
+
+	if buttons.cross then
+		wlan.connect()
+		ftp.init()
+	end
+
+	if buttons.triangle then usbMassStorage() end
+
+	if buttons.square then
+		ftp.term()
+		os.restart()
+	end
+
+	if buttons.released.start then break end -- Exit
+
+end
+ftp.term()

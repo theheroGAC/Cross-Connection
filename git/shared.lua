31ffa596c5dc79ad9c02914137490f86f696bd10
theheroGAC <gamesandconsolestaff@gmail.com>
2018-01-17 21:50:38 +0100

    Add files via upload

---
+++ git/shared.lua
@@ -0,0 +1,9 @@
+-- Constants
+
+APP_REPO = "theheroGAC"
+APP_PROJECT = "Cross Connection"			--here name of repo in github...your vpk is QuickLaunch_installer_for_PSVita.vpk
+
+APP_VERSION_MAJOR = 0x01 -- major.minor
+APP_VERSION_MINOR = 0x01
+
+APP_VERSION = ((APP_VERSION_MAJOR << 0x18) | (APP_VERSION_MINOR << 0x10)) -- Union Binary
